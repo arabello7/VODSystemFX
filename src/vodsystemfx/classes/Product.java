595e14ff1c75f00eeee0102e18bb797c1a6a9377
arabello7 <tomekj97@gmail.com>
2019-01-04 20:57:45 +0100

    Working on product window

--- src/vodsystemfx/classes/Product.java
+++ src/vodsystemfx/classes/Product.java
@@ -5,6 +5,12 @@
  */
 package vodsystemfx.classes;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 /**
  *
  * @author tomas
@@ -18,9 +24,52 @@ public class Product {
     protected int productionDate; //type?
     protected int duration;
     protected Distributor distributor; //Losuj z dostepnych
-    protected String[] prodCountries;
+    protected String[] prodCountries = new String[3]; //dobrze?
     protected double usersRating; //Na poczatku zero, potem sie zmienia, ludzie losowo glosuja
+    
+    protected final int COUNTRIESSIZE = 199; // Countires in text file
     //actors ?
+
+    public String readFile(int line) {
+        BufferedReader reader;
+        String text = "";
+        try {
+            reader = new BufferedReader(new FileReader("textfiles/countires.txt"));
+            while (line > 0){
+                reader.readLine();
+                line--;
+            }
+            text = reader.readLine();
+            reader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File Not Found!");
+        } catch (IOException ex) {
+            System.out.println("In/Out Exception");
+        }
+        return text;
+    }
+    
+    public void randomizeProduct() {
+            System.out.println("Random product:");
+        photo = "?";
+        //name - randomowe 3 slowa
+        Random rand = new Random();
+        price = rand.nextDouble() * 20;
+            System.out.println("price: " + price);
+        description = "lorem ipsum..."; // albo uniwersalna historia na podstawie wylosowanych pol
+        //dystrybutor wybór z listy dostępnych
+        
+        //prod countires losowanie 3 z pliku
+        int line1 = rand.nextInt(199);
+        int line2 = rand.nextInt(199);
+        int line3 = rand.nextInt(199);
+        
+        prodCountries[0] = readFile(line1);
+        prodCountries[1] = readFile(line2);
+        prodCountries[2] = readFile(line3);
+
+        usersRating = 0.0;
+    }
 
     public Product() {
 //        UserInterface.getProductInfo(photo, name, price, ..., productionDate, duration, prodCountries, ...);
